fd584c520fbfb14ded40144fc9e70a3132de250d
James <james.ashton.nichols@gmail.com>
2014-07-08 18:48:47 +1000

    fixed up site_collection class to be able to process with an adjacency matrix, it's actually a bit ugly now, but works

--- reaction_diffusion.py
+++ reaction_diffusion.py
@@ -3,31 +3,10 @@ import numpy as np
 
 import pdb
 
-#set parameters for reaction-diffusion
-I = 0.5
-a = I-1 
-b = 1
-c = - 1
-d = I
-mu = 0.25
-nu = mu
-t = [0,0.01]
-#t = range(0,100,1)
-
-#define differential equations for reaction-diffusion
-def dfdt(state, t, nbstate, a, b, c, d, mu, nu):
-            dsdt = np.zeros(len(state))
-            dsdt[0] = a*state[0] + b*state[1] + mu*(nbstate[0]-2*state[0])
-            dsdt[1] = c*state[0] + d*state[1] + mu*(nbstate[1]-2*state[1])
-            #for i in range(0,len(self.state)):
-             #   dsdt[i] = 
-            return dsdt
-#integrate
-
 class Site(object):
     """ define class  Site: a "Site" is an individual cell in a Turing system, 
     ok? i shuold probably have called it a cell. I still need to include docstrings etc"""
-    def __init__(self,location,state, a=0.5, b=1., c=-1., d=0.5, mu=0.25, nu=0.25):
+    def __init__(self,location, state, a=0.5, b=1., c=-1., d=0.5, mu=0.25, nu=0.25):
         self.loc = location
         self.state = state
         self.a = a
@@ -64,6 +43,8 @@ class SiteCollection(object):
     def __init__(self,sites, soln_times=np.linspace(0.0,1.0,101)):
         self.Sites = sites
         self.build_adjacency_matrix()
+        print self.adjacency
+        self.t = soln_times
 
     def build_adjacency_matrix(self):
         """ Takes the sites collection and builds an adjacency matrix """
@@ -77,8 +58,13 @@ class SiteCollection(object):
                 if self.Sites[j] in self.Sites[i].nb:
                     self.adjacency[i,j] = True
     
-    #def get_site_vector(self):
-    #    return [site.State for site in self.Sites]
+    def build_param_vectors(self):
+        self.a_vec = [site.a for site in self.Sites]
+        self.b_vec = [site.b for site in self.Sites]
+        self.c_vec = [site.c for site in self.Sites]
+        self.d_vec = [site.d for site in self.Sites]
+        self.mu_vec = [site.mu for site in self.Sites]
+        self.nu_vec = [site.nu for site in self.Sites]
 
     def update(self):
         for s in self.Sites:
@@ -86,30 +72,28 @@ class SiteCollection(object):
         for s in self.Sites:
             s.update()
     
-    def better_update(self):
-        self.state = scipy.integrate.odeint(dfdt, self.state, t, args = (self.nbstate,a,b,c,d,mu,nu))[1]        
+    def solve(self):
+        
+        # Stupidly odeint can only operate on functions that work with 1-D arrays, so we have to use the flattened vector of morphogens
+        states = np.array([site.state for site in self.Sites]).transpose().flatten()
+        self.build_param_vectors()
+        
+        pdb.set_trace() 
+        self.solution = scipy.integrate.odeint(self.ensemble_dfdt, states, self.t)        
+        
+        return self.solution
          
-    def ensemble_dfdt(self):
-        dsdt = np.zeros(len(state))
-        
-        a_vec = [site.a for site in self.Sites]
-        b_vec = [site.b for site in self.Sites]
-        c_vec = [site.c for site in self.Sites]
-        d_vec = [site.d for site in self.Sites]
-        mu_vec = [site.mu for site in self.Sites]
-        nu_vec = [site.nu for site in self.Sites]
-        
-        states = [site.State for site in self.Sites]
-        
-        pdb.set_trace()
-        dfdt[0] = a_vec * states[0,:] + b * states[1,:] + mu * (dot(self.adjacency * states[0,:]) - self.adjacency.sum(1) * states[0,:])
-        dfdt[1] = c_vec * states[0,:] + b * states[1,:] + mu * (dot(self.adjacency * states[1,:]) - self.adjacency.sum(1) * states[1,:])
+    def ensemble_dfdt(self, states, t):
+        dfdt = np.zeros(len(states))
 
+        # Stupidly odeint can only operate on functions that work with 1-D arrays, so we have to use the flattened vector of morphogens
+        morph_1 = states[:len(self.Sites)]
+        morph_2 = states[len(self.Sites):]
         
-        #for i in range(0,len(self.state)):
-         #   dsdt[i] = 
+        dfdt[:len(self.Sites)] = self.a_vec * morph_1 + self.b_vec * morph_2 + self.mu_vec * (np.dot(self.adjacency, morph_1) - self.adjacency.sum(1) * morph_1)
+        dfdt[len(self.Sites):] = self.c_vec * morph_1 + self.d_vec * morph_2 + self.nu_vec * (np.dot(self.adjacency, morph_2) - self.adjacency.sum(1) * morph_2)
+        
         return dfdt
-        
             
     def __str__(self):
         return '<'  + str(self.Sites) + '>'
