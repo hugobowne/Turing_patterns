6b1f5440d68a2186583bd5f96286a2a4ab55e5f6
James-Nichols <james.ashton.nichols@gmail.com>
2015-05-02 09:48:54 +1000

    doesn't fail but doesn't do much either

--- image_reaction_diffusion.py
+++ image_reaction_diffusion.py
@@ -28,37 +28,39 @@ warnings.filterwarnings('ignore', category=RuntimeWarning)
 
 def array_to_img(img_array, w, h):
     # reconstruct image here. Can't quite use reshape by itself...
-    r = xs[:len(img_array)/3].reshape(w,h)
-    g = xs[len(img_array)/3:2*len(xs)/3].reshape(w,h)
-    b = xs[2*len(img_array)/3:].reshape(w,h)
+    r = img_array[:len(img_array)/3].reshape(h,w)
+    g = img_array[len(img_array)/3:2*len(img_array)/3].reshape(h,w)
+    b = img_array[2*len(img_array)/3:].reshape(h,w)
 
-    return np.append(np.append(r, g), b)
-
-def array_to_img(r, g, b):
-    r = 
+    return np.dstack([r,g,b])
 
 # Here we define out reaction diffusion system
 # t is time, y the state vector (corresponds to both Y_r and X_r in Turing's paper), and a the set of coefficients
-def colour_diffuse(xs, t, D_r, D_g, D_b, w, h):
+def colour_diffuse(img, t, D, w, h):
     
     # dfdt template is stupid - reconstruct img shape here
-    r = xs[:len(xs)/3].reshape(w,h)
-    g = xs[len(xs)/3:2*len(xs)/3].reshape(w,h)
-    b = xs[2*len(xs)/3:].reshape(w,h)
+    #r = img[:len(img)/3].reshape(w,h)
+    #g = img[len(img)/3:2*len(img)/3].reshape(w,h)
+    #b = img[2*len(img)/3:].reshape(w,h)
 
-    # The vector xs consists of X_r in the first half, and Y_r in the second
-    drdt = np.zeros(r.shape)
-    dgdt = np.zeros(g.shape)
-    dbdt = np.zeros(b.shape)
-        
-    for i in range(w):
-        for j in range(h):
-            drdt[i,j] = D_r * 0.25 * (r[i-1,j] + r[i+1,j] + r[i,j-1] + r[i,j+1] - 4.*r[i,j]) 
-            dgdt[i,j] = D_g * 0.25 * (g[i-1,j] + g[i+1,j] + g[i,j-1] + g[i,j+1] - 4.*g[i,j]) 
-            dbdt[i,j] = D_b * 0.25 * (b[i-1,j] + b[i+1,j] + b[i,j-1] + b[i,j+1] - 4.*b[i,j]) 
+    # The vector img consists of X_r in the first half, and Y_r in the second
+    #drdt = np.zeros(r.shape)
+    #dgdt = np.zeros(g.shape)
+    #dbdt = np.zeros(b.shape)
+    didt = np.zeros(img.shape)
+    print "integrating at", t 
+    for k in range(3):
+        for i in range(1,h-1):
+            for j in range(1,w-1):
+                didt[i*w + j + k*w*h] = D[k] * 0.25 * (img[(i-1)*w + j + k*w*h] + img[(i+1)*w + j + k*w*h] \
+                                        + img[i*w + (j-1) + k*w*h] + img[i*w + (j+1) + k*w*h] - 4.*img[i*w + j + k*w*h]) 
 
-    result = np.append(np.append(drdt.flatten(), dgdt.flatten()), dbdt.flatten())
-    return result
+                #drdt[i,j] = D_r * 0.25 * (r[i-1,j] + r[i+1,j] + r[i,j-1] + r[i,j+1] - 4.*r[i,j]) 
+                #dgdt[i,j] = D_g * 0.25 * (g[i-1,j] + g[i+1,j] + g[i,j-1] + g[i,j+1] - 4.*g[i,j]) 
+                #dbdt[i,j] = D_b * 0.25 * (b[i-1,j] + b[i+1,j] + b[i,j-1] + b[i,j+1] - 4.*b[i,j]) 
+
+    #result = np.append(np.append(drdt.flatten(), dgdt.flatten()), dbdt.flatten())
+    return didt 
 
 #img = misc.imread("james_hugo.jpg")
 img = Image.open("james_hugo.jpg")
@@ -66,18 +68,16 @@ img_array = np.array(img)
 # Flatten 3 2D channels in to one vector...
 img_flat = np.append(np.append(img_array[:,:,0].flatten(), img_array[:,:,1].flatten()), img_array[:,:,2].flatten())
 
-t = np.linspace(0., 1., 500)
+t = np.linspace(0., 1., 5)
+dX = 0.05
+D_r = dX*0.01
+D_g = dX*0.1
+D_b = dX*0.02
 
-D_r = 0.01
-D_g = 0.1
-D_b = 0.2
+(h, w) = img_array[:,:,0].shape
 
-img_states = scipy.integrate.odeint(colour_diffuse, img_flat, t, args = (D_r, D_g, D_b))
-    
-r = xs[:len(xs)/3].reshape(w,h)
-g = xs[len(xs)/3:2*len(xs)/3].reshape(w,h)
-b = xs[2*len(xs)/3:].reshape(w,h)
-
-
-img_states[-1]
-
+img_states = scipy.integrate.odeint(colour_diffuse, img_flat, t, args = ([D_r, D_g, D_b], w, h))
+final_img_array = array_to_img(img_states[-1], w, h).astype(np.uint8)
+pdb.set_trace()
+final_img = Image.fromarray(final_img_array)
+final_img.save('processed.jpg')
