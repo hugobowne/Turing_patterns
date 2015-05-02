f43619559d2b130a44664a16966e8d5f8add5ac0
James-Nichols <james.ashton.nichols@gmail.com>
2015-05-02 13:22:36 +1000

    working version with diffusion and chemotaxis. No reactions yet

--- image_reaction_diffusion.py
+++ image_reaction_diffusion.py
@@ -34,32 +34,29 @@ def array_to_img(img_array, w, h):
 
     return np.dstack([r,g,b])
 
+def laplacian(y, i, j, w, h, k):
+    # Take Laplacian of vectorised 2-d field, with three species (i.e. k=0,1,2)
+    return 0.25 * (y[(i-1)*w + j + k*w*h] + y[(i+1)*w + j + k*w*h] \
+            + y[i*w + (j-1) + k*w*h] + y[i*w + (j+1) + k*w*h] - 4.*y[i*w + j + k*w*h]) 
+    
+
 # Here we define out reaction diffusion system
 # t is time, y the state vector (corresponds to both Y_r and X_r in Turing's paper), and a the set of coefficients
 def colour_diffuse(img, t, D, w, h):
     
-    # dfdt template is stupid - reconstruct img shape here
-    #r = img[:len(img)/3].reshape(w,h)
-    #g = img[len(img)/3:2*len(img)/3].reshape(w,h)
-    #b = img[2*len(img)/3:].reshape(w,h)
-
-    # The vector img consists of X_r in the first half, and Y_r in the second
-    #drdt = np.zeros(r.shape)
-    #dgdt = np.zeros(g.shape)
-    #dbdt = np.zeros(b.shape)
     didt = np.zeros(img.shape)
     print "integrating at", t 
-    for k in range(3):
-        for i in range(1,h-1):
-            for j in range(1,w-1):
-                didt[i*w + j + k*w*h] = D[k] * 0.25 * (img[(i-1)*w + j + k*w*h] + img[(i+1)*w + j + k*w*h] \
-                                        + img[i*w + (j-1) + k*w*h] + img[i*w + (j+1) + k*w*h] - 4.*img[i*w + j + k*w*h]) 
-
-                #drdt[i,j] = D_r * 0.25 * (r[i-1,j] + r[i+1,j] + r[i,j-1] + r[i,j+1] - 4.*r[i,j]) 
-                #dgdt[i,j] = D_g * 0.25 * (g[i-1,j] + g[i+1,j] + g[i,j-1] + g[i,j+1] - 4.*g[i,j]) 
-                #dbdt[i,j] = D_b * 0.25 * (b[i-1,j] + b[i+1,j] + b[i,j-1] + b[i,j+1] - 4.*b[i,j]) 
-
-    #result = np.append(np.append(drdt.flatten(), dgdt.flatten()), dbdt.flatten())
+    for i in range(1,h-1):
+        for j in range(1,w-1):
+        
+            ddx2 = np.array([laplacian(img, i, j, w, h, k) for k in range(3)])
+            # This multiply gives us the diffusion forces as well as chemotactic forces
+            forces = np.dot(D, ddx2)
+            
+            for k in range(3):
+                # First we just add the diffusion part of the function, i.e. laplacian of each colour
+                didt[i*w + j + k*w*h] = forces[k]
+            
     return didt 
 
 #img = misc.imread("james_hugo.jpg")
@@ -69,15 +66,30 @@ img_array = np.array(img)
 img_flat = np.append(np.append(img_array[:,:,0].flatten(), img_array[:,:,1].flatten()), img_array[:,:,2].flatten())
 
 t = np.linspace(0., 1., 5)
-dX = 0.05
+dX = 500.0
+
+# These are the diffusion terms
 D_r = dX*0.01
 D_g = dX*0.1
 D_b = dX*0.02
 
+# These are the chemotaxis terms: -ve means attraction, +ve means repulsion
+n_rg = 0.      # Attraction of red towards green
+n_rb = dX*0.05 # Attraction of red towards blue...
+n_gr = -dX*0.03
+n_gb = 0.
+n_br = 0.
+n_bg = 0.
+
+D = dX * np.array([[D_r, n_rg, n_rb], [n_gr, D_g, n_gb], [n_br, n_bg, D_b]] )
+
+# Need the width and the height of the image to process the vector
 (h, w) = img_array[:,:,0].shape
 
-img_states = scipy.integrate.odeint(colour_diffuse, img_flat, t, args = ([D_r, D_g, D_b], w, h))
+img_states = scipy.integrate.odeint(colour_diffuse, img_flat, t, args = (D, w, h))
 final_img_array = array_to_img(img_states[-1], w, h).astype(np.uint8)
-pdb.set_trace()
+
 final_img = Image.fromarray(final_img_array)
 final_img.save('processed.jpg')
+diff_img = Image.fromarray(final_img_array - img_array)
+diff_img.save('difference.jpg')
